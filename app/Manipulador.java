7981ee8ff0101437d130e8710bab1fd03744674c
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-01 20:55:54 -0300

    :hammer: Adição do tratamento de entrada do tipo de carta

--- app/Manipulador.java
+++ app/Manipulador.java
@@ -1,8 +1,11 @@
 package app;
 
 import java.util.InputMismatchException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
+import error.CartaInexistente;
 import error.EntradaNegativo;
 import error.ExcessoJogador;
 import error.ExcessoRodadas;
@@ -12,6 +15,7 @@ import src.Jogador;
 public class Manipulador {
     private int quantidadeRodadas = 3;
     private int quantidadeJogadores;
+    private int tipoCarta;
 
     Scanner leitor = new Scanner(System.in);
 
@@ -21,12 +25,19 @@ public class Manipulador {
         this.quantidadeRodadas = novaQuantidadeRodadas;
     }
 
-    public void setQuantidadeJogadores(int novaQuantidadeJogadores) throws ExcessoJogador, EntradaNegativo {
+    private void setQuantidadeJogadores(int novaQuantidadeJogadores) throws ExcessoJogador, EntradaNegativo {
         if (novaQuantidadeJogadores > 5) throw new ExcessoJogador();
         if (novaQuantidadeJogadores < 0) throw new EntradaNegativo();
         this.quantidadeJogadores = novaQuantidadeJogadores;
     }
 
+    private void setTipoCarta(int novoTipoCarta) throws CartaInexistente, EntradaNegativo {
+        novoTipoCarta--;
+        if (novoTipoCarta > 2) throw new CartaInexistente();
+        if (novoTipoCarta < 0) throw new EntradaNegativo();
+        this.tipoCarta = novoTipoCarta;
+    }
+    
     private void criarJogador(String nome){
         try {
             Jogador player = new Jogador(nome);
@@ -82,8 +93,37 @@ public class Manipulador {
         }
     }
 
+    private void trataEntradaTipoCarta(String mensagemEntrada){
+        List<String> cartas = new ArrayList<>();
+        cartas.add("Carta Normal");
+        cartas.add("Carta Naipe");
+        cartas.add("Carta Valor");
+
+        while (true) {
+            try {
+                for (String carta : cartas) {
+                    System.out.println("[" + (cartas.indexOf(carta)+1) + "] " + carta);;
+                }
+                System.out.print(mensagemEntrada);
+                this.setTipoCarta(leitor.nextInt());
+                break;
+
+            } catch (InputMismatchException error) {
+                System.out.println("Insira apenas os números das opções.");
+                continue;
+            } catch (EntradaNegativo error) {
+                System.out.println("Não pode inserir inteiros negativos.");
+                continue;
+            } catch (CartaInexistente error) {
+                System.out.println("O tipo de carta inserida não existe.");
+                continue;
+            }
+        }       
+    }
+
     public Manipulador(){
         this.trataEntradaQuantidadeRodadas("Quantas rodadas (padrão é 3)? ");
         this.trataEntradaQuantidadeJogadores("Quantos jogadores (máximo de 5)? ");
+        this.trataEntradaTipoCarta("Qual o tipo de carta do jogo? ");
     }
 }
