93ea5227c7eeb11be7898f09ac51ccf9c5dff36a
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-11 00:23:33 -0300

    :hammer_and_wrench: Adição de javadoc no Main.java

--- app/Main.java
+++ app/Main.java
@@ -1,5 +1,8 @@
 package app;
 
+/**
+ * Classe responsável pela ativação do Controlador do jogo.
+ */
 public class Main {
     public static void main(String[] args) {
         Controlador appMain  = new Controlador();
