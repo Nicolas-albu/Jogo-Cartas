0f282941e525a2953002368b7009a75402324fec
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-11 00:26:32 -0300

    :hammer_and_wrench: Adição de javadoc e da geração de pontuação da rodada no construtor da carta

--- src/CartaFactory/CartaNormal.java
+++ src/CartaFactory/CartaNormal.java
@@ -3,14 +3,21 @@ package src.CartaFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Classe que representa uma carta do tipo normal.
+ */
 public class CartaNormal extends Carta {
     private String nomeNaipe;
     private int valorNaipe;
 
+    /**
+     * Cria uma nova carta normal.
+     */
     public CartaNormal() {
         super();
         this.geraValorNaipe();
         this.geraNomeNaipe();
+        this.geraPontuacaoRodada();
     }
 
     @Override
@@ -43,14 +50,23 @@ public class CartaNormal extends Carta {
         this.geraNomeNaipe();
     }
 
+    /**
+     * @return retorna o nome do naipe da carta.
+     */
     private String getNomeNaipe() {
         return this.nomeNaipe;
     }
 
+    /**
+     * Gera o valor do naipe da carta normal.
+     */
     private void geraValorNaipe() {
         this.valorNaipe = geradorAleatorio.nextInt(3, 6);
     }
 
+    /**
+     * Gera o nome do naipe da carta normal.
+     */
     private void geraNomeNaipe() {
         Map<Integer, String> naipes = new HashMap<>() {
             {
